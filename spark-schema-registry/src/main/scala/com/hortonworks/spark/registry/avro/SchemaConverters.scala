d3525a243595ef6c395ba2feb84f311f2297f718
Arun Mahadevan <arunm@apache.org>
2019-05-20 15:02:53 -0700

    Revert "BUG-112730: Fixes for Spark 2.4"
    This reverts commit 860b17b4af7a799d230a25d6090b0b6c6691bcc2.
    
    HDP-3.1-maint branch reverted Spark version to 2.3.2 so reverting
    this commit.

--- spark-schema-registry/src/main/scala/com/hortonworks/spark/registry/avro/SchemaConverters.scala
+++ spark-schema-registry/src/main/scala/com/hortonworks/spark/registry/avro/SchemaConverters.scala
@@ -184,8 +184,10 @@ object SchemaConverters {
         builder.map()
           .values(toAvroType(vt, valueContainsNull, recordName, nameSpace))
       case st: StructType =>
-        val childNameSpace = if (nameSpace != "") s"$nameSpace.$recordName" else recordName
-        val fieldsAssembler = builder.record(recordName).namespace(nameSpace).fields()
+        val curNameSpace = if (nameSpace != "") nameSpace else recordName
+        val childNameSpace = s"$curNameSpace.$recordName"
+
+        val fieldsAssembler = builder.record(recordName).namespace(curNameSpace).fields()
         st.foreach { f =>
           val fieldAvroType =
             toAvroType(f.dataType, f.nullable, f.name, childNameSpace)
